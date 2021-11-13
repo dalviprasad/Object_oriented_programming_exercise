ad558dc05f82c632d1ad56ed20554039f39509e5
Prasad Dalvi <dalviprasad1995@gmail.com>
2021-11-13 22:01:49 +0530

    Update a2_task1.py

--- a2_task1.py
+++ a2_task1.py
@@ -1,34 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[44]:
-
-
 # Task 1 code template for FIT9136 Assignment 2.
-# Instructions to students:
-# 1. Where required, replace the *pass* statements with your own method and
-#   function definitions. Your submission must comply with the specification:
-#   do not rename specified functions or change the number or type of arguments
-#   or return values; otherwise you will be deemed not to have demonstrated
-#   clear comprehension of the specified instructions. (You may define
-#   your own additional functions and instance variables if you wish, as long
-#   as these don't contradict the specified requirements. You may also
-#   import any libraries allowed by the assignment specification.)
-# 2. Complete task 1 within the framework of this template file.
-# 3. Modify the filename (AND import statement(s), where required) to replace
-#   the xxxxxxxx with your student ID number.
-# 4. Complete tasks 2 and 3 within the other template files. The finished
-#   program is split into three files, linked together by import statements.
-# 5. In this file, you may define your own testing code within the 'main'
-#   block to check if your simulation is working when running this script file
-#   directly. Code in the main block will not be run by the automarker algorithm,
-#   which will instead test the specified functions/methods by attempting to
-#   call them directly according to how they are defined in the assignment 
-#   requirments specification.
-# 6. Before submission, you should remove these instructions from this
-#   template file and add your own program comments instead. This template file
-#   has omitted program comments, which are your responsibility to add. Any
-#   other 'placeholder' comments should be removed from your final submission.
 
 class Person: # Creating a class name Person and making functions for different inputs as mentioned in the pdf
 
